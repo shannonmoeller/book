fdfa14158c69aab1e3565c41d7dfd392bb2a617a
Shannon Moeller <me@shannonmoeller.com>
2012-05-30 12:25:04 -0300

    Cleaned up whitespace. Resolved jslint errors.

--- book.js
+++ book.js
@@ -1,117 +1,117 @@
 /*jslint regexp: true */
 (function (global) {
-    'use strict';
+	'use strict';
 
-    var doc = global.document,
-        els = doc.querySelectorAll('script[src*=book\\.js]'),
-        attr = els[els.length - 1].src.replace(/^[^\?]*\?/, ''),
-        args = global.decodeURIComponent(attr).split(/[\s+]+/),
-        log = false,
+	var doc = global.document,
+		els = doc.querySelectorAll('script[src*=book\\.js]'),
+		attr = els[els.length - 1].src.replace(/^[^\?]*\?/, ''),
+		args = global.decodeURIComponent(attr).split(/[\s+]+/),
+		log = false,
 
-        exec = function (obj) {
-            if (args.length) {
-                obj[args.shift()]();
-            }
-        },
+		exec = function (obj) {
+			if (args.length) {
+				obj[args.shift()]();
+			}
+		},
 
-        go = function (url) {
-            url += args.join('/');
+		go = function (url) {
+			url += args.join('/');
 
-            if (log) {
-                global.console.log(url);
-                return;
-            }
+			if (log) {
+				global.console.log(url);
+				return;
+			}
 
-            global.location.href = url;
-        },
-        
-        load = function (url) {
-            return function () {
-                var el = doc.createElement('script');
+			global.location.href = url;
+		},
 
-                doc.body.appendChild(el).src = url;
-            }
-        },
+		load = function (url) {
+			return function () {
+				var el = doc.createElement('script');
 
-        toGo = function (url) {
-            return function () {
-                go(url);
-            };
-        };
+				doc.body.appendChild(el).src = url;
+			};
+		},
 
-    exec({
-        // Do or do not.
-        log: function () {
-            log = true;
+		toGo = function (url) {
+			return function () {
+				go(url);
+			};
+		};
 
-            exec(this);
-        },
-        
-    	// HTML5 Support
-    	5: function () {
-            exec({   
-            	c: toGo('http://caniuse.com/'),
-                d: toGo('http://html5doctor.com/'),
-                p: toGo('http://html5please.com/'),
-                t: toGo('http://html5test.com/')
-            });
-    	},
-    
-    	// Bookmarklets
-    	b: function () {
-            exec({
-                a: load('http://erkie.github.com/asteroids.min.js'),
-            	d: load('http://mir.aculo.us/dom-monster/dommonster.js'),
-                f: load('http://getfirebug.com/firebug-lite.js'),
-                j: load('http://code.jquery.com/jquery.js')
-            });
-    	},
-        
-    	// Google
-    	g: function () {
-            var url = 'http://google.com/';
-            
-            if (args.length) {
-        		url += 'search?q=';
-                url += args.join('+');
-            }
-            
-            go(url);
-    	},
-    
-    
-    	// Mozilla Developer Network
-    	m: function () {
-    		var url = 'http://developer.mozilla.org/';
-            
-            if (args.length) {
-            	url += 'en-US/search?q=';
-                url += args.join('+');
-            }
-            
-            go(url);
-    	},
-        
-        // Social
-        s: function () {
-            exec({
-                f: toGo('http://facebook.com/shannonmoeller/'),
-    	        l: toGo('http://linkedin.com/in/shannonmoeller/'),
-            	t: toGo('http://twitter.com/shannonmoeller/')
-            });
-        },
-    
-    	// Wikipedia
-    	w: function (u) {
-    		var url = 'http://en.wikipedia.org/');
-            
-            if (args.length) {
-                url += 'wiki/Special:Search?search=';
-                url += args.join('+');
-            }
-            
-            go(url);
-    	}
-    });
+	exec({
+		// Do or do not.
+		log: function () {
+			log = true;
+
+			exec(this);
+		},
+
+		// HTML5 Support
+		5: function () {
+			exec({
+				c: toGo('http://caniuse.com/'),
+				d: toGo('http://html5doctor.com/'),
+				p: toGo('http://html5please.com/'),
+				t: toGo('http://html5test.com/')
+			});
+		},
+
+		// Bookmarklets
+		b: function () {
+			exec({
+				a: load('http://erkie.github.com/asteroids.min.js'),
+				d: load('http://mir.aculo.us/dom-monster/dommonster.js'),
+				f: load('http://getfirebug.com/firebug-lite.js'),
+				j: load('http://code.jquery.com/jquery.js')
+			});
+		},
+
+		// Google
+		g: function () {
+			var url = 'http://google.com/';
+
+			if (args.length) {
+				url += 'search?q=';
+				url += args.join('+');
+			}
+
+			go(url);
+		},
+
+
+		// Mozilla Developer Network
+		m: function () {
+			var url = 'http://developer.mozilla.org/';
+
+			if (args.length) {
+				url += 'en-US/search?q=';
+				url += args.join('+');
+			}
+
+			go(url);
+		},
+
+		// Social
+		s: function () {
+			exec({
+				f: toGo('http://facebook.com/shannonmoeller/'),
+				l: toGo('http://linkedin.com/in/shannonmoeller/'),
+				t: toGo('http://twitter.com/shannonmoeller/')
+			});
+		},
+
+		// Wikipedia
+		w: function () {
+			var url = 'http://en.wikipedia.org/';
+
+			if (args.length) {
+				url += 'wiki/Special:Search?search=';
+				url += args.join('+');
+			}
+
+			go(url);
+		}
+	});
 
 }(this));
